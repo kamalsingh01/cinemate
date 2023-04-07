05328612f16e8884e8893593a4479745a2fcac90
kamalsingh01 <communk1@gmail.com>
2023-04-08 00:20:19 +0530

    Key Updated, changes in settings.py

--- cinemate/settings.py
+++ cinemate/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9fto0!3%7br3+0w-32!knjvx90p$x4%yzx88j6@6t1d1%ef%u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
